3f3a1e45783e78ae8b31537827c90db0cd86ade4
youkorr <96953496+youkorr@users.noreply.github.com>
2025-08-31 05:01:14 +0200

    Update touchscreen.py

--- components/gsl3680/touchscreen.py
+++ components/gsl3680/touchscreen.py
@@ -1,20 +1,19 @@
 from esphome import pins
 import esphome.codegen as cg
-from esphome.components import touchscreen, i2c
+from esphome.components import i2c, touchscreen
 import esphome.config_validation as cv
 from esphome.const import (
     CONF_ID, 
     CONF_INTERRUPT_PIN, 
     CONF_RESET_PIN,
-    CONF_ADDRESS,
 )
 
-DEPENDENCIES = ["i2c"]
-
 ns_ = cg.esphome_ns.namespace("gsl3680")
+
 cls_ = ns_.class_(
     "GSL3680",
     touchscreen.Touchscreen,
+    i2c.I2CDevice,
 )
 
 CONFIG_SCHEMA = (
@@ -23,30 +22,17 @@ CONFIG_SCHEMA = (
         {
             cv.GenerateID(): cv.declare_id(cls_),
             cv.Required(CONF_INTERRUPT_PIN): pins.internal_gpio_input_pin_schema,
-            cv.Required(CONF_RESET_PIN): pins.internal_gpio_output_pin_schema,
-            cv.Optional(CONF_ADDRESS, default=0x40): cv.i2c_address,
+            cv.Required(CONF_RESET_PIN): pins.internal_gpio_input_pin_schema,
         }
     )
     .extend(i2c.i2c_device_schema(0x40))
 )
 
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await touchscreen.register_touchscreen(var, config)
-    
-    # Récupération de la configuration I2C d'ESPHome
-    i2c_bus = await cg.get_variable(config["i2c_id"])
-    cg.add(var.set_i2c_bus(i2c_bus))
-    cg.add(var.set_i2c_address(config[CONF_ADDRESS]))
-    
-    # Configuration des pins
-    if CONF_INTERRUPT_PIN in config:
-        interrupt_pin = await cg.gpio_pin_expression(config[CONF_INTERRUPT_PIN])
-        cg.add(var.set_interrupt_pin(interrupt_pin))
-    
-    if CONF_RESET_PIN in config:
-        reset_pin = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
-        cg.add(var.set_reset_pin(reset_pin))
-    
-    # Ajout spécial pour ESP32-P4 : contournement du conflit I2C
-    cg.add_define("GSL3680_ESP32P4_WORKAROUND")
+    await i2c.register_i2c_device(var, config)
+
+    cg.add(var.set_interrupt_pin(await cg.gpio_pin_expression(config.get(CONF_INTERRUPT_PIN))))
+    cg.add(var.set_reset_pin(await cg.gpio_pin_expression(config.get(CONF_RESET_PIN))))
