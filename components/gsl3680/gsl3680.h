c8e6c53e1b79c5f626ccca967f1ce8ccfb744eb6
youkorr <96953496+youkorr@users.noreply.github.com>
2025-08-31 04:56:47 +0200

    Update gsl3680.h

--- components/gsl3680/gsl3680.h
+++ components/gsl3680/gsl3680.h
@@ -1,57 +1,36 @@
-// gsl3680.h
 #pragma once
-
+#include "esphome/core/application.h"
 #include "esphome/core/component.h"
-#include "esphome/core/hal.h"
+#include "esphome/core/helpers.h"
 #include "esphome/components/touchscreen/touchscreen.h"
-#include "esphome/components/i2c/i2c.h"
+#include "esphome/core/hal.h"
+#include "esp_lcd_gsl3680.h"
 
 namespace esphome {
 namespace gsl3680 {
 
-static const char *const TAG = "touchscreen.gsl3680";
+constexpr static const char *const TAG = "touchscreen.gsl3680";
 
 class GSL3680 : public touchscreen::Touchscreen {
- public:
-  void setup() override;
-  void loop() override;
-  void update_touches() override;
+    public:
+        void setup() override;
+        void update_touches() override;
+        void set_interrupt_pin(InternalGPIOPin *pin) { this->interrupt_pin_ = pin; }
+        void set_reset_pin(InternalGPIOPin *pin) { this->reset_pin_ = pin; }
+        void set_i2c_bus(int bus_num) { this->i2c_bus_num_ = bus_num; }
+        void set_i2c_address(uint8_t address) { this->i2c_address_ = address; }
 
-  void set_interrupt_pin(InternalGPIOPin *interrupt_pin) { interrupt_pin_ = interrupt_pin; }
-  void set_reset_pin(InternalGPIOPin *reset_pin) { reset_pin_ = reset_pin; }
-  void set_i2c_address(uint8_t address) { i2c_address_ = address; }
-  void set_i2c_bus(i2c::I2CBus *bus) { i2c_bus_ = bus; }
-
- protected:
-  void reset_();
-  bool get_touches_(uint8_t &touches, uint16_t *x, uint16_t *y);
-
-  // -----------------------------
-  // Méthodes I2C pour ESPHome 2025
-  // -----------------------------
-  bool read_byte_i2c(uint8_t reg, uint8_t *data);
-  bool read_bytes_i2c(uint8_t reg, uint8_t *data, size_t len);
-
-#ifdef GSL3680_ESP32P4_WORKAROUND
-  bool read_byte_workaround_(uint8_t reg, uint8_t *data);
-  bool read_bytes_workaround_(uint8_t reg, uint8_t *data, size_t len);
-#endif
-
-  InternalGPIOPin *interrupt_pin_{nullptr};
-  InternalGPIOPin *reset_pin_{nullptr};
-  uint8_t i2c_address_{0x40};
-  i2c::I2CBus *i2c_bus_{nullptr};
-  bool setup_complete_{false};
-
-#ifdef GSL3680_ESP32P4_WORKAROUND
-  // Variables pour contourner le conflit I2C sur ESP32-P4
-  uint8_t sda_pin_{7};
-  uint8_t scl_pin_{8};
-  uint32_t frequency_{100000};
-  void *native_i2c_handle_{nullptr};
-#endif
+    protected:
+        InternalGPIOPin *interrupt_pin_{};
+        InternalGPIOPin *reset_pin_{};
+        int i2c_bus_num_{0};
+        uint8_t i2c_address_{0x40}; // Adresse par défaut du GSL3680
+        size_t width_{1280};
+        size_t height_{800};
+        esp_lcd_touch_handle_t tp_{};
+        esp_lcd_panel_io_handle_t tp_io_handle_{};
 };
 
-}  // namespace gsl3680
-}  // namespace esphome
+} // namespace gsl3680
+} // namespace esphome
 
