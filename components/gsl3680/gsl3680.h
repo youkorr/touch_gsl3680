18635d103f8eb8e9f1210b3601a54726668cafd1
youkorr <96953496+youkorr@users.noreply.github.com>
2025-09-01 01:23:05 +0200

    Update gsl3680.h

--- components/gsl3680/gsl3680.h
+++ components/gsl3680/gsl3680.h
@@ -2,35 +2,41 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
-#include "esphome/components/i2c/i2c.h"
 #include "esphome/components/touchscreen/touchscreen.h"
-#include "esp_lcd_gsl3680.h"
+#include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
 namespace gsl3680 {
 
-static const char *const TAG = "touchscreen.gsl3680";
-
-class GSL3680 : public touchscreen::Touchscreen, public i2c::I2CDevice {
+class GSL3680 : public touchscreen::Touchscreen, public i2c::I2CDevice, public Component {
  public:
   void setup() override;
+  void dump_config() override;
   void update_touches() override;
-
+  
   void set_interrupt_pin(InternalGPIOPin *pin) { this->interrupt_pin_ = pin; }
   void set_reset_pin(InternalGPIOPin *pin) { this->reset_pin_ = pin; }
 
  protected:
+  void reset_();
+  bool read_touch_data_();
+  void handle_interrupt_();
+  
   InternalGPIOPin *interrupt_pin_{nullptr};
   InternalGPIOPin *reset_pin_{nullptr};
-
-  size_t width_ = 1280;
-  size_t height_ = 800;
-
-  esp_lcd_touch_handle_t tp_{nullptr};
-  esp_lcd_panel_io_handle_t tp_io_handle_{nullptr};
+  
+  // Données de touch
+  struct TouchPoint {
+    uint16_t x;
+    uint16_t y;
+    uint8_t pressure;
+    bool valid;
+  };
+  
+  TouchPoint touches_[10];  // Support jusqu'à 10 points
+  uint8_t touch_count_{0};
+  
+  static const char *const TAG;
 };
-
-}  // namespace gsl3680
-}  // namespace esphome
 
 
