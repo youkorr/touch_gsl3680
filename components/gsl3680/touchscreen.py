0993648ce53e4fc1a0d7b5e4b5d61d77b49ec4dd
youkorr <96953496+youkorr@users.noreply.github.com>
2025-08-31 07:19:51 +0200

    Update touchscreen.py

--- components/gsl3680/touchscreen.py
+++ components/gsl3680/touchscreen.py
@@ -3,12 +3,14 @@ import esphome.codegen as cg
 from esphome.components import i2c, touchscreen
 import esphome.config_validation as cv
 from esphome.const import (
-    CONF_ID, 
-    CONF_INTERRUPT_PIN, 
+    CONF_ID,
+    CONF_INTERRUPT_PIN,
     CONF_RESET_PIN,
 )
 
 DEPENDENCIES = ["i2c"]
+
+# Namespace pour le composant
 ns_ = cg.esphome_ns.namespace("gsl3680")
 
 cls_ = ns_.class_(
@@ -26,7 +28,18 @@ CONFIG_SCHEMA = (
             cv.Required(CONF_RESET_PIN): pins.internal_gpio_input_pin_schema,
         }
     )
-    .extend(i2c.i2c_device_schema(0x40))
+    .extend(i2c.i2c_device_schema(0x40))  # adresse par défaut 0x40
+)
+
+# Champs supplémentaires pour driver_ng
+CONF_SDA = "sda_pin"
+CONF_SCL = "scl_pin"
+
+CONFIG_SCHEMA = CONFIG_SCHEMA.extend(
+    {
+        cv.Required(CONF_SDA): pins.internal_gpio_output_pin_schema,
+        cv.Required(CONF_SCL): pins.internal_gpio_output_pin_schema,
+    }
 )
 
 
@@ -35,5 +48,12 @@ async def to_code(config):
     await touchscreen.register_touchscreen(var, config)
     await i2c.register_i2c_device(var, config)
 
+    # Pins d'interruption et reset
     cg.add(var.set_interrupt_pin(await cg.gpio_pin_expression(config.get(CONF_INTERRUPT_PIN))))
     cg.add(var.set_reset_pin(await cg.gpio_pin_expression(config.get(CONF_RESET_PIN))))
+
+    # SDA/SCL et adresse I2C pour driver_ng
+    cg.add(var.set_sda_pin(config[CONF_SDA]))
+    cg.add(var.set_scl_pin(config[CONF_SCL]))
+    cg.add(var.set_address(config.get("address", 0x40)))  # 0x40 par défaut
+
