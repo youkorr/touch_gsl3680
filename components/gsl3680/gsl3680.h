09fa32e6e38fb77f63049060a43d6ade99cc2635
youkorr <96953496+youkorr@users.noreply.github.com>
2025-08-31 04:20:57 +0200

    Update gsl3680.h

--- components/gsl3680/gsl3680.h
+++ components/gsl3680/gsl3680.h
@@ -1,32 +1,34 @@
 #pragma once
 
+#include "esphome/core/application.h"
 #include "esphome/core/component.h"
-#include "esphome/core/hal.h"
-#include "esphome/components/touchscreen/touchscreen.h"
+#include "esphome/core/helpers.h"
 #include "esphome/components/i2c/i2c.h"
+#include "esphome/components/touchscreen/touchscreen.h"
+#include "esphome/core/hal.h"
+#include "esp_lcd_gsl3680.h"
 
 namespace esphome {
 namespace gsl3680 {
 
-static const char *const TAG = "touchscreen.gsl3680";
+constexpr static const char *const TAG = "touchscreen.gsl3680";
 
 class GSL3680 : public touchscreen::Touchscreen, public i2c::I2CDevice {
- public:
-  void setup() override;
-  void loop() override;
-  void update_touches() override;
+    public:
+        void setup() override;
+        void update_touches() override;
 
-  void set_interrupt_pin(InternalGPIOPin *interrupt_pin) { interrupt_pin_ = interrupt_pin; }
-  void set_reset_pin(InternalGPIOPin *reset_pin) { reset_pin_ = reset_pin; }
+        void set_interrupt_pin(InternalGPIOPin *pin) { this->interrupt_pin_ = pin; }
+        void set_reset_pin(InternalGPIOPin *pin) { this->reset_pin_ = pin; }
 
- protected:
-  void reset_();
-  bool get_touches_(uint8_t &touches, uint16_t *x, uint16_t *y);
-  
-  InternalGPIOPin *interrupt_pin_{nullptr};
-  InternalGPIOPin *reset_pin_{nullptr};
-  bool setup_complete_{false};
+    protected:
+        InternalGPIOPin *interrupt_pin_{};
+        InternalGPIOPin *reset_pin_{};
+        size_t width_ = 1280;
+        size_t height_ = 800;
+        esp_lcd_touch_handle_t tp_{};
+        esp_lcd_panel_io_handle_t tp_io_handle_{};
 };
 
-}  // namespace gsl3680
-}  // namespace esphome
+}
+}
