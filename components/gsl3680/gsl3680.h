ffc32cf624cfb9c156e05c9821650e3eb31b0ec8
youkorr <96953496+youkorr@users.noreply.github.com>
2025-09-01 01:27:20 +0200

    Update gsl3680.h

--- components/gsl3680/gsl3680.h
+++ components/gsl3680/gsl3680.h
@@ -8,7 +8,7 @@
 namespace esphome {
 namespace gsl3680 {
 
-class GSL3680 : public touchscreen::Touchscreen, public i2c::I2CDevice, public Component {
+class GSL3680 : public touchscreen::Touchscreen, public i2c::I2CDevice {
  public:
   void setup() override;
   void dump_config() override;
@@ -18,25 +18,19 @@ class GSL3680 : public touchscreen::Touchscreen, public i2c::I2CDevice, public C
   void set_reset_pin(InternalGPIOPin *pin) { this->reset_pin_ = pin; }
 
  protected:
-  void reset_();
-  bool read_touch_data_();
-  void handle_interrupt_();
+  void reset_controller_();
+  bool read_touch_();
+  void IRAM_ATTR handle_interrupt_();
   
   InternalGPIOPin *interrupt_pin_{nullptr};
   InternalGPIOPin *reset_pin_{nullptr};
   
-  // Données de touch
-  struct TouchPoint {
-    uint16_t x;
-    uint16_t y;
-    uint8_t pressure;
-    bool valid;
-  };
-  
-  TouchPoint touches_[10];  // Support jusqu'à 10 points
-  uint8_t touch_count_{0};
+  bool touch_detected_{false};
   
   static const char *const TAG;
 };
+
+}  // namespace gsl3680
+}  // namespace esphome
 
 
