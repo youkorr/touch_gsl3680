4fcdb3ce57cd143ce94781d24a148cb2070316db
youkorr <96953496+youkorr@users.noreply.github.com>
2025-08-31 05:06:21 +0200

    Update gsl3680.cpp

--- components/gsl3680/gsl3680.cpp
+++ components/gsl3680/gsl3680.cpp
@@ -4,36 +4,13 @@ namespace esphome {
 namespace gsl3680 {
 
 void GSL3680::setup() {
-    ESP_LOGI(TAG, "Setting up GSL3680 touchscreen");
-    
-    // Configuration de l'interface I2C pour le touchscreen
-    esp_lcd_panel_io_i2c_config_t tp_io_config = {
-        .dev_addr = this->address_,
-        .control_phase_bytes = 1,
-        .dc_bit_offset = 0,
-        .lcd_cmd_bits = 0,
-        .lcd_param_bits = 8,
-        .flags = {
-            .dc_low_on_data = 0,
-            .disable_control_phase = 1,
-        },
-    };
-    
-    ESP_LOGI(TAG, "Initialize touch IO (I2C) on bus %d, address 0x%02X", this->bus_num_, this->address_);
-    
-    // Utilisation de l'API ESP-IDF native
+
+    esp_lcd_panel_io_i2c_config_t tp_io_config = ESP_LCD_TOUCH_IO_I2C_GSL3680_CONFIG();
+    ESP_LOGI(TAG, "Initialize touch IO (I2C)");
     esp_err_t ret = esp_lcd_new_panel_io_i2c((esp_lcd_i2c_bus_handle_t)this->bus_num_, &tp_io_config, &this->tp_io_handle_);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to create I2C panel IO: %s", esp_err_to_name(ret));
-        this->mark_failed();
-        return;
-    }
-    
-    // Configuration des dimensions selon l'orientation
-    this->x_raw_max_ = this->swap_x_y_ ? this->get_display()->get_native_height() : this->get_display()->get_native_width();
-    this->y_raw_max_ = this->swap_x_y_ ? this->get_display()->get_native_width() : this->get_display()->get_native_height();
-    
-    // Configuration du contrôleur tactile
+    this->x_raw_max_ = this->swap_x_y_? this->get_display()->get_native_height(): this->get_display()->get_native_width() ;
+    this->y_raw_max_ = this->swap_x_y_? this->get_display()->get_native_width() : this->get_display()->get_native_height();
+
     esp_lcd_touch_config_t tp_cfg = {
         .x_max = this->get_display()->get_native_width(),
         .y_max = this->get_display()->get_native_height(),
@@ -48,55 +25,33 @@ void GSL3680::setup() {
             .mirror_y = 0,
         },
     };
-    
-    ESP_LOGI(TAG, "Initialize touch controller GSL3680");
-    ret = esp_lcd_touch_new_i2c_gsl3680(this->tp_io_handle_, &tp_cfg, &this->tp_);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to create GSL3680 touch: %s", esp_err_to_name(ret));
-        this->mark_failed();
-        return;
-    }
-    
-    // Configuration de la broche d'interruption si définie
-    if (this->interrupt_pin_ != nullptr) {
-        this->interrupt_pin_->setup();
-        this->attach_interrupt_(this->interrupt_pin_, gpio::INTERRUPT_FALLING_EDGE);
-    }
-    
-    ESP_LOGI(TAG, "GSL3680 setup completed successfully");
+
+    ESP_LOGI(TAG, "Initialize touch controller gsl3680");
+    ESP_ERROR_CHECK(esp_lcd_touch_new_i2c_gsl3680(this->tp_io_handle_, &tp_cfg, &this->tp_));
+
+    this->interrupt_pin_->setup();
+    this->attach_interrupt_(this->interrupt_pin_, gpio::INTERRUPT_ANY_EDGE);
+
 }
 
 void GSL3680::update_touches() {
-    if (this->tp_ == nullptr) {
-        return;
-    }
-    
     uint16_t x[CONFIG_ESP_LCD_TOUCH_MAX_POINTS];
     uint16_t y[CONFIG_ESP_LCD_TOUCH_MAX_POINTS];
     uint16_t touch_strength[CONFIG_ESP_LCD_TOUCH_MAX_POINTS];
-    uint8_t touch_cnt = 0;
-    
-    // Lecture des données tactiles
-    esp_err_t ret = esp_lcd_touch_read_data(this->tp_);
-    if (ret != ESP_OK) {
-        ESP_LOGW(TAG, "Failed to read touch data: %s", esp_err_to_name(ret));
-        return;
-    }
-    
-    // Récupération des coordonnées
-    bool touchpad_pressed = esp_lcd_touch_get_coordinates(this->tp_, x, y, touch_strength, &touch_cnt, CONFIG_ESP_LCD_TOUCH_MAX_POINTS);
-    
-    if (touchpad_pressed && touch_cnt > 0) {
+    uint8_t touch_cnt;
+    esp_lcd_touch_read_data(this->tp_);
+    bool touchpad_pressed = esp_lcd_touch_get_coordinates(this->tp_, (uint16_t*)&x, (uint16_t*)&y, (uint16_t*)&touch_strength, &touch_cnt, CONFIG_ESP_LCD_TOUCH_MAX_POINTS);
+    if (touchpad_pressed) {
         for (int i = 0; i < touch_cnt; i++) {
-            ESP_LOGV(TAG, "Touch point [%d]: x=%d, y=%d, strength=%d", i, x[i], y[i], touch_strength[i]);
+            ESP_LOGV(TAG, "GSL3680::update_touches: [%d] %dx%d - %d, %d", i, x[i], y[i], touch_strength[i], touch_cnt);
             this->add_raw_touch_position_(i, x[i], y[i]);
         }
+        this->add_raw_touch_position_(0, x[0], y[0]); // Second touch coords are weird
     }
 }
 
-} // namespace gsl3680
-} // namespace esphome
-
+}
+}
 
 
 
